4df4c02d34cf6ac93fab5a843ff33e4f32de86b2
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2023-12-12 15:44:12 +0530

    eslint error removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StartRating from "./StarRating";
+// import StartRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
